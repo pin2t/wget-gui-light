b9971c3adf70e104ed6cff47d280338356c6fe96
kot <samoylovnn@gmail.com>
2014-08-28 01:20:21 +0600

    Update lastversion.js

--- lastversion.js
+++ lastversion.js
@@ -1,10 +1,12 @@
 var web_wgetguilight = {
     "version": "0.0.12",
-    "name": "Wget gui light v0.0.12",
-    
-    "description": "Some security fixes",
-    "descriptionUrl": "https://github.com/tarampampam/nod32-update-mirror",
-    
-    "downloadPage": "https://github.com/tarampampam/nod32-update-mirror",
-    "downloadUrl": "https://github.com/tarampampam/nod32-update-mirror/archive/master.zip"
+    "name": "Wget GUI light v0.0.12",
+    "lastUpdate": {
+        "shortDesc": "Some security fixes, new features",
+        "fullDescUrl": "https://github.com/tarampampam/nod32-update-mirror"
+    }
+    "download": {
+        "page": "https://github.com/tarampampam/wget-gui-light/blob/master/HowUpdate.md",
+        "file": "https://github.com/tarampampam/nod32-update-mirror/archive/master.zip"
+    }
 };
