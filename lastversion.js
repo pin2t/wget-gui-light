c592907c7350312145d375f813a53ae6e56f3646
kot <samoylovnn@gmail.com>
2014-08-31 15:11:47 +0600

    Some security fixes

--- lastversion.js
+++ lastversion.js
@@ -1,8 +1,8 @@
 var web_wgetguilight = {
-    "version": "0.1.0",
+    "version": "0.1.1",
     "name": "Wget GUI light",
     "lastUpdate": {
-        "shortDesc": "Added history, fixed Google Chrome extension, fixed filenames with Cyrillic, other fixes",
+        "shortDesc": "Some security fixes",
         "fullDescUrl": "https://github.com/tarampampam/wget-gui-light/blob/master/cahngeslog.md"
     },
     "download": {
